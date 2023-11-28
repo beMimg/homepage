1027a19cbc6a2551fc94d9c610914c065a8e697a
bernyig <bernardommguerreiro@gmail.com>
2023-11-28 23:26:54 +0100

    header done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import generateJoke from './generateJoke';
-import './styles/main.scss';
-import githubIconSvg from './assets/github.svg';
-
-const githubSvg = document.getElementById('github-svg');
-githubSvg.src = githubIconSvg;
-
-console.log(generateJoke());
-console.log('hddi');
+import walkingPng from './assets/walking.png';
+import githubSvg from './assets/github.svg';
+import linkedinSvg from './assets/linkedin.svg';
+import './styles/style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const bundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -15,7 +12,6 @@ module.exports = {
       filename: 'index.html',
       template: 'src/template.html',
     }),
-    new BundleAnalyzerPlugin(),
   ],
   devtool: 'inline-source-map',
   devServer: {
