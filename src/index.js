a55c257376537352c78447182b50d4cfc7e6dab4
bernyig <bernardommguerreiro@gmail.com>
2023-11-29 19:26:11 +0100

    footer desktop complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import walkingPng from './assets/walking.png';
 import githubSvg from './assets/github.svg';
 import linkedinSvg from './assets/linkedin.svg';
+import mailboxSvg from './assets/mailbox.svg';
+import phoneSvg from './assets/phone.svg';
+import keyboardJpg from './assets/keyboard.jpg';
 import './styles/style.css';
